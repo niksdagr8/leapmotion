f8183a792a7f2c92b62242e45d3069ff0d7257e7
HRCM Group <nikhilso@andrew.cmu.edu>
2013-08-14 22:50:37 -0500

    initial commit

---
+++ build/CMakeFiles/leap.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/leap.dir/src/Sample.o"
+  "../bin/leap.pdb"
+  "../bin/leap"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/leap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
